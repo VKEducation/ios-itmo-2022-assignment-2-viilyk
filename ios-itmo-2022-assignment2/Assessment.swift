1023fd9fd0141c6bc3d26795c5e6bd9e69a28c71
Marlesss <334874@niuitmo.ru>
2022-10-14 18:36:08 +0300

    hw2.0

---
+++ ios-itmo-2022-assignment2/Assessment.swift
@@ -0,0 +1,86 @@
+
+import Foundation
+import UIKit
+
+class Assessment: UIControl {
+    
+    private lazy var stars: [UIButton] = {
+        var stars: [UIButton] = []
+        for i in 0..<5 {
+            stars.append(UIButton())
+            stars[i].setImage(UIImage(named: "Star.png"), for: .normal)
+            stars[i].setImage(UIImage(named: "Union.png"), for: .highlighted)
+            stars[i].addTarget(self, action: #selector(didTapButton), for: .touchDown)
+            stars[i].translatesAutoresizingMaskIntoConstraints = false
+        }
+        return stars
+    }()
+
+    private lazy var label:UILabel = {
+        let label = UILabel()
+        label.text = "Ваша оценка"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = UIColor(red: 189/255, green: 189/255, blue: 189/255, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private var mark = [0: "Ужасно", 1: "Плохо", 2: "Нормально", 3: "Хорошо", 4: "AMAZING!"]
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        for i in 0..<5 {
+            addSubview(stars[i])
+        }
+        addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            
+            stars[0].topAnchor.constraint(equalTo: topAnchor),
+            stars[0].leadingAnchor.constraint(equalTo: leadingAnchor),
+            
+            stars[1].topAnchor.constraint(equalTo: topAnchor),
+            stars[1].leadingAnchor.constraint(equalTo: stars[0].trailingAnchor, constant: 11),
+            
+            stars[2].topAnchor.constraint(equalTo: topAnchor),
+            stars[2].leadingAnchor.constraint(equalTo: stars[1].trailingAnchor, constant: 11),
+            
+            stars[3].topAnchor.constraint(equalTo: topAnchor),
+            stars[3].leadingAnchor.constraint(equalTo: stars[2].trailingAnchor, constant: 11),
+            
+            stars[4].topAnchor.constraint(equalTo: topAnchor),
+            stars[4].leadingAnchor.constraint(equalTo: stars[3].trailingAnchor, constant: 11),
+            stars[4].trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            label.topAnchor.constraint(equalTo: stars[4].bottomAnchor, constant: 20),
+            label.heightAnchor.constraint(equalToConstant: 19),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor),
+            label.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
+    
+    
+    @objc private func didTapButton(_ button: UIButton) {
+        var number = 0
+        while button != stars[number] {
+            number+=1
+        }
+        for i in 0...number {
+            stars[i].setImage(UIImage(named: "Union.png"), for: .normal)
+        }
+        for i in (number + 1)..<5 {
+            stars[i].setImage(UIImage(named: "Star.png"), for: .normal)
+        }
+        label.text = mark[number]
+        isSelected = true
+        sendActions(for: .editingChanged)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+}
+
--- ios-itmo-2022-assignment2/ViewController.swift
+++ ios-itmo-2022-assignment2/ViewController.swift
@@ -1,19 +1,94 @@
-//
-//  ViewController.swift
-//  ios-itmo-2022-assignment2
-//
-//  Created by rv.aleksandrov on 29.09.2022.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Фильм"
+        label.font = .boldSystemFont(ofSize: 30)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var name: Container = Container(frame: CGRect(), text: "Название", placeholder: "Название фильма")
+    
+    private lazy var director: Container = Container(frame: CGRect(), text: "Режисёр", placeholder: "Режисёр фильма")
+    
+    private lazy var year: Container = Container(frame: CGRect(), text: "Год", placeholder: "Год выпуска")
+    
+    private lazy var assessment: Assessment = Assessment()
+    
+    private lazy var datePicker: UIDatePicker = UIDatePicker()
+    
+    private lazy var save: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Сохранить", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red: 93/255, green: 176/255, blue: 117/255, alpha: 1)
+        button.layer.cornerRadius = 25.5
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.isEnabled = false
+        button.layer.opacity = 0.4
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(label)
+        view.addSubview(name)
+        view.addSubview(director)
+        view.addSubview(year)
+        view.addSubview(assessment)
+        view.addSubview(save)
+        //year.textField.inputView = datePicker
+        //datePicker.datePickerMode = .date
+        assessment.translatesAutoresizingMaskIntoConstraints = false
+        
+        name.addTarget(self, action: #selector(containerDidChange), for: .editingChanged)
+        director.addTarget(self, action: #selector(containerDidChange), for: .editingChanged)
+        year.addTarget(self, action: #selector(containerDidChange), for: .editingChanged)
+        assessment.addTarget(self, action: #selector(containerDidChange), for: .editingChanged)
+
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            label.heightAnchor.constraint(equalToConstant: 36),
+            
+            name.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 40),
+            name.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            name.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            director.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 16),
+            director.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            director.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            year.topAnchor.constraint(equalTo: director.bottomAnchor, constant: 16),
+            year.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            year.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            assessment.topAnchor.constraint(equalTo: year.bottomAnchor, constant: 40),
+            assessment.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            save.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            save.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            save.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            save.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            save.heightAnchor.constraint(equalToConstant: 51)
+        ])
     }
-
-
+    
+    @objc
+    private func containerDidChange() {
+        if name.isSelected && director.isSelected && year.isSelected && assessment.isSelected {
+            save.isEnabled = true
+            save.layer.opacity = 1
+        } else {
+            save.isEnabled = false
+            save.layer.opacity = 0.4
+        }
+    }
 }
-
